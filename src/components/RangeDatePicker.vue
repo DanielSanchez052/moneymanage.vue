fd1f4dbeecf76a364970d07592d80fae6c3bbce6
DanielSanchez052 <sanchezd0528@gmail.com>
2023-12-15 23:07:31 -0500

    change label of date picker

--- src/components/RangeDatePicker.vue
+++ src/components/RangeDatePicker.vue
@@ -11,7 +11,7 @@
             name="start" 
             type="text" 
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Start date"
+            placeholder="Desde"
         >
     </div>
     <span class="mx-2 text-gray-500 dark:text-gray-400">to</span>
@@ -26,7 +26,7 @@
             name="end" 
             type="text"     
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="End date">
+            placeholder="Hasta">
     </div>
 </div>
 </template>
