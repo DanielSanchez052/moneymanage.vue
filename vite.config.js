cd4347a9f885bce8159b1f0ead3b6e3bc2516a3b
DanielSanchez052 <sanchezd0528@gmail.com>
2023-12-18 21:56:50 -0500

    remove mkcert

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true },
-  plugins: [mkcert(), vue()],
+  server: {},
+  plugins: [vue()],
   resolve: {
     alias : [
       { find: '@', replacement:  fileURLToPath(new URL("./src", import.meta.url)) }
